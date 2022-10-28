4bb76cbc39b6dcf66ee5ee713bb0d29c4722c022
Julio <dbjulioj@gmai.com>
2022-10-28 12:32:21 -0400

    Parte final

---
+++ proyectolistbox/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace proyectolistbox
+{
+    partial class proyectlistbox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnQuitar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.entradatextbox = new System.Windows.Forms.TextBox();
+            this.mostrarlistbox = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(396, 146);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(137, 50);
+            this.btnAgregar.TabIndex = 0;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnQuitar
+            // 
+            this.btnQuitar.Location = new System.Drawing.Point(396, 202);
+            this.btnQuitar.Name = "btnQuitar";
+            this.btnQuitar.Size = new System.Drawing.Size(137, 50);
+            this.btnQuitar.TabIndex = 1;
+            this.btnQuitar.Text = "Quitar";
+            this.btnQuitar.UseVisualStyleBackColor = true;
+            this.btnQuitar.Click += new System.EventHandler(this.btnQuitar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(396, 258);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(137, 50);
+            this.btnBorrar.TabIndex = 2;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(396, 314);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(137, 50);
+            this.btnSalir.TabIndex = 3;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // entradatextbox
+            // 
+            this.entradatextbox.Location = new System.Drawing.Point(396, 110);
+            this.entradatextbox.Multiline = true;
+            this.entradatextbox.Name = "entradatextbox";
+            this.entradatextbox.Size = new System.Drawing.Size(137, 30);
+            this.entradatextbox.TabIndex = 4;
+            // 
+            // mostrarlistbox
+            // 
+            this.mostrarlistbox.FormattingEnabled = true;
+            this.mostrarlistbox.ItemHeight = 16;
+            this.mostrarlistbox.Location = new System.Drawing.Point(189, 110);
+            this.mostrarlistbox.Name = "mostrarlistbox";
+            this.mostrarlistbox.Size = new System.Drawing.Size(180, 228);
+            this.mostrarlistbox.TabIndex = 5;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // proyectlistbox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mostrarlistbox);
+            this.Controls.Add(this.entradatextbox);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnQuitar);
+            this.Controls.Add(this.btnAgregar);
+            this.Name = "proyectlistbox";
+            this.Text = "Proyectlistbox";
+            this.Load += new System.EventHandler(this.proyectlistbox_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnQuitar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.TextBox entradatextbox;
+        private System.Windows.Forms.ListBox mostrarlistbox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ proyectolistbox/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyectolistbox
+{
+    public partial class proyectlistbox : Form
+    {
+        public proyectlistbox()
+        {
+            InitializeComponent();
+        }
+
+        private void proyectlistbox_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void btnQuitar_Click(object sender, EventArgs e)
+        {
+            if (mostrarlistbox.SelectedIndex != -1)
+                mostrarlistbox.Items.RemoveAt(mostrarlistbox.SelectedIndex);
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            mostrarlistbox.Items.Add(entradatextbox.Text);
+            entradatextbox.Clear();
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            mostrarlistbox.Items.Clear();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
